128ae55a5b07a2ddcee75da3e1d3519aa0afc415
PREETHIVIRAJ M <20eumt057@skcet.ac.in>
2025-07-22 13:45:40 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -3,7 +3,7 @@
 # Set paths
 INVENTORY_PATH="/home/ansible/slaves.txt"
 PLAYBOOK1="/opt/docker/ansible-build-push-docker.yml"
-PLAYBOOK2="/opt/dokcer/ansible-pull-run-docker.yml"
+PLAYBOOK2="/opt/docker/ansible-pull-run-docker.yml"
 
 # Run Ansible Playbook
 ansible-playbook -i "$INVENTORY_PATH" "$PLAYBOOK1";
